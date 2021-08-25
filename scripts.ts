0cef20d10b13bfbb1fb28226b94ed8a7aabc5e68
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-08-25 16:46:10 +0200

    Styles

---
+++ scripts.ts
@@ -0,0 +1,14 @@
+const objects = document.querySelectorAll('object') as NodeListOf<HTMLObjectElement>;
+
+const settingsPage = {
+    height: '100vh',
+    width: 'calc(100vw - 20px)'
+}
+
+objects.forEach(page => page.style.height = '100vh');
+// objects.forEach(page => page.style = { ...page.style, settingsPage });
+// objects.forEach(page => Object.assign(page.style,settingsPage));
+objects.forEach(page => {
+    console.log(page);
+    Object.assign(page.style, settingsPage)
+});
